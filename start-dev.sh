483de14f12bb1ac7ff85640755ad0be298a907d8
Anthony Pilloud <pilloud.anthony@gmail.com>
2019-03-01 09:53:44 +0000

    [edit] start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -33,53 +33,6 @@ all() {
     updatedb
 }
 
-install() {
-    aptdep
-
-    if [ "$1" = "dev" ]
-    then
-        setTitre "Installation lancée en mode DEV (des paramètres seront installés en plus -> cron, cronfig hosts local)"
-    fi
-
-    mkdir tmp
-    mkdir data
-
-    setTitre "Installation des dépendances PHP7.2"
-    sudo apt-get -y install php7.2-mysql php7.2-mbstring
-
-    setTitre "Ajout de permissions sur le binaire"
-    sudo chmod +x bin/console
-
-    if [ "$1" = "dev" ]
-    then
-        setTitre "mise en place de la configuration dans /etc/hosts"
-        last_host=tail  /etc/hosts -n 1
-
-        if grep --quiet ruined-world.local /etc/hosts
-        then
-            setTitre "la config lms est déjà présente dans /etc/hosts"
-        else
-            sudo -- sh -c -e 'echo "127.0.0.1  ruined-world.local\c" >> /etc/hosts'
-        fi
-
-        setTitre "mise en place de la configuration du cron"
-        if sudo crontab -l | grep -q 'ruined-world.local'
-        then
-            setTitre "la config lms dans le cron roo est déjà présente"
-        else
-            line="*/1 * * * * curl ruined-world.local/cron"
-            (sudo crontab -u root -l; echo "$line" ) | sudo crontab -u root -
-        fi
-    fi
-
-    restartservice
-
-    composeupdate
-    checkcache
-    chmodfiles
-    updatedb
-}
-
 checkcache() {
     setTitre "Vidage du cache"
     sudo rm -rf var/cache/*
@@ -105,7 +58,7 @@ chmodfiles() {
     sudo chmod -R 765 tmp
     sudo chown -R www-data:www-data data
     sudo chown -R www-data:www-data tmp
-    sudo chown -R 1000:1000 public
+    sudo chown -R 1002:1002 public
 }
 
 restartservice() {
