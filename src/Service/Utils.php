84563732dda4e9dd325434cf1acca54ba8423efc
Piou-piou <pilloud.anthony@gmail.com>
2019-03-04 20:29:22 +0100

    [func] add method to create recursive directories from root folder

--- src/Service/Utils.php
+++ src/Service/Utils.php
@@ -19,4 +19,32 @@ class Utils
 	{
 		$this->container = $container;
 	}
+	
+	/**
+	 * method that create a tree of folders on each slash
+	 *
+	 * @param $path
+	 * @return string
+	 */
+	public function createRecursiveDirFromRoot($path)
+	{
+		$fs = new Filesystem();
+		$root_dir = $this->container->get("kernel")->getProjectDir();
+		$new_path = $root_dir;
+		$folders = explode("/", $path);
+		
+		foreach ($folders as $index => $folder) {
+			$new_path .= $folder;
+			
+			if (!$fs->exists($new_path)) {
+				$fs->mkdir($new_path);
+			}
+			
+			if ($index + 1 < count($folders)) {
+				$new_path .= "/";
+			}
+		}
+		
+		return $new_path;
+	}
 }
