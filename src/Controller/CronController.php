05f4f18289699014564ce37f157ba36d7415ab30
Piou-piou <pilloud.anthony@gmail.com>
2019-03-19 17:01:21 +0100

    [fix] use in CronController

--- src/Controller/CronController.php
+++ src/Controller/CronController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Base;
 use App\Service\Globals;
+use App\Service\Resources;
 use App\Service\Utils;
 use Cron\CronExpression;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
