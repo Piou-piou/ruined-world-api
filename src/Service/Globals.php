2de5617998c542df1b626ccca303e839f481d9c9
Piou-piou <pilloud.anthony@gmail.com>
2019-03-20 09:53:35 +0100

    [edit] globals to get token and base on request parameters or session

--- src/Service/Globals.php
+++ src/Service/Globals.php
@@ -6,6 +6,7 @@ use App\Entity\Base;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Globals
@@ -25,11 +26,52 @@ class Globals
 	 */
 	private $session;
 	
-	public function __construct(ContainerInterface $container, EntityManagerInterface $em, SessionInterface $session)
+	/**
+	 * @var \Symfony\Component\HttpFoundation\Request|null
+	 */
+	private $request;
+	
+	public function __construct(ContainerInterface $container, EntityManagerInterface $em, SessionInterface $session, RequestStack $request_stack)
 	{
 		$this->container = $container;
 		$this->em = $em;
 		$this->session = $session;
+		$this->request = $request_stack->getCurrentRequest();
+	}
+	
+	/**
+	 * method that set session for base and token base on request parameters
+	 * if no parameters in request test if the sessions already exists.
+	 * if nothing exist send false else true
+	 * @param string|null $token
+	 * @param string|null $jwt
+	 * @return bool
+	 */
+	private function setBaseAndToken(?string $token, ?string $jwt): bool
+	{
+		if ($token !== null && $jwt !== null) {
+			$json = Jwt::decode($jwt, $token);
+			
+			if ($json !== false) {
+				$user = $this->em->getRepository(User::class)->findOneBy(["token" => $token]);
+				
+				if (!$user) return false;
+				
+				$current_base = $this->em->getRepository(Base::class)->findOneBy([
+					"id" => $json["id_base"],
+					"user" => $user
+				]);
+				$this->em->refresh($current_base);
+				$this->session->set("current_base", $current_base);
+				$this->session->set("token", $token);
+			}
+			
+			return false;
+		} else if (!$this->session->has("current_base") && !$this->session->has("token")) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 	/**
@@ -39,11 +81,10 @@ class Globals
 	 */
 	public function getCurrentBase($force_refresh = false)
 	{
-		$user = $this->em->getRepository(User::class)->findOneBy(["token" => $this->session->get("token")]);
+		if ($this->setBaseAndToken($this->request->get("token"), $this->request->get("jwt")) === false) return false;
 		
-		if ($this->session->has("current_base") === true && $user !== null) {
+		if ($this->session->has("current_base") === true && $this->session->has("token") === true) {
 			if ($force_refresh) {
-				
 				$current_base = $this->em->getRepository(Base::class)->find($this->session->get("current_base")->getId());
 				$this->em->refresh($current_base);
 				$this->session->set("current_base", $current_base);
