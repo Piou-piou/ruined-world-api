c7b7c32c42919efbd0650bc56e9f074d405200d3
Piou-piou <pilloud.anthony@gmail.com>
2019-07-03 20:07:13 +0200

    [fix] typoe in url of getting units

--- src/Controller/BarrackController.php
+++ src/Controller/BarrackController.php
@@ -12,7 +12,7 @@ class BarrackController extends AbstractController
 {
 	/**
 	 * metho to send units that are possible to recruit
-	 * @Route("/api/barrack/ist-to-recruit/", name="list_units_to_recruit", methods={"POST"})
+	 * @Route("/api/barrack/list-units-to-recruit/", name="list_units_to_recruit", methods={"POST"})
 	 * @param Session $session
 	 * @param Globals $globals
 	 * @return JsonResponse
