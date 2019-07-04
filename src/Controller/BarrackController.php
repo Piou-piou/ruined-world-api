c3e538f7f788dca8bc87a7657b19366b39c78f3d
Piou-piou <pilloud.anthony@gmail.com>
2019-07-04 08:53:13 +0200

    [func] add method to recruit units

--- src/Controller/BarrackController.php
+++ src/Controller/BarrackController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Unit;
+use App\Service\Barrack;
 use App\Service\Globals;
+use DateInterval;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -24,5 +28,51 @@ class BarrackController extends AbstractController
 			"units" => $globals->getUnitsConfig(),
 			"token" => $session->get("user")->getToken(),
 		]);
+	}
+
+	/**
+	 * method to create unit and set time of their recruitment
+	 * @Route("/api/barrack/recruit-units/", name="recruit_units", methods={"POST"})
+	 * @param Session $session
+	 * @param Globals $globals
+	 * @param Barrack $barrack
+	 * @return JsonResponse
+	 * @throws Exception
+	 */
+	public function recruitUnits(Session $session, Globals $globals, Barrack $barrack): JsonResponse
+	{
+		$em = $this->getDoctrine()->getManager();
+		$now = new \DateTime();
+		$infos = $session->get("jwt_infos");
+		$unit_array_name = $infos->unit_array_name;
+		$number_to_recruit = (int)$infos->number_to_recruit;
+		$unit_config = $globals->getUnitsConfig()[$unit_array_name];
+		$end_recruitment = $now->add(new DateInterval("PT" . $unit_config["recruitment_time"] . "S"));
+
+		if ($barrack->testWithdrawResourcesToRecruit($unit_array_name, $number_to_recruit)) {
+			for ($i = 0; $i < $number_to_recruit; $i++) {
+				$unit = new Unit();
+				$unit->setName($unit_config["name"]);
+				$unit->setArrayName($unit_config["array_name"]);
+				$unit->setAssaultLevel(1);
+				$unit->setDefenseLevel(1);
+				$unit->setBase($globals->getCurrentBase());
+				$unit->setInRecruitment(true);
+				$unit->setEndRecruitment($end_recruitment);
+				$em->persist($unit);
+			}
+
+			$em->flush();
+
+			return new JsonResponse([
+				"success" => true,
+				"success_message" => "Les unités demandées sont en cours de recrutement"
+			]);
+		} else {
+			return new JsonResponse([
+				"success" => false,
+				"error_message" => "Il n'y a pas assez de ressources dans la base pour recruter autant d'unités"
+			]);
+		}
 	}
 }
