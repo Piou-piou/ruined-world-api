d3eebd3fb769e26b2346c9cc1065619af6560a5c
Piou-piou <pilloud.anthony@gmail.com>
2019-07-09 17:33:01 +0200

    [func] prepare send current unit movements in base

--- src/Controller/UnitsController.php
+++ src/Controller/UnitsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Unit;
+use App\Entity\UnitMovement;
+use App\Service\Api;
 use App\Service\Globals;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -25,6 +27,22 @@ class UnitsController extends AbstractController
 			"success" => true,
 			"token" => $session->get("user")->getToken(),
 			"units" => $units
+		]);
+	}
+
+	/**
+	 * @Route("/api/units/list-movements/", name="units_list_movements", methods={"POST"})
+	 * @param SessionInterface $session
+	 * @param Globals $globals
+	 * @param \App\Service\Unit $unit
+	 * @return JsonResponse
+	 */
+	public function sendUnitsInMovement(SessionInterface $session, Globals $globals, \App\Service\Unit $unit): JsonResponse
+	{
+		return new JsonResponse([
+			"success" => true,
+			"token" => $session->get("user")->getToken(),
+			"unit_movements" => $unit->getCurrentMovementsInBase()
 		]);
 	}
 }
---
+++ src/Service/Unit.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\UnitMovement;
+use Doctrine\ORM\EntityManagerInterface;
+
+class Unit
+{
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $em;
+
+	/**
+	 * @var Globals
+	 */
+	private $globals;
+
+	/**
+	 * Unit constructor.
+	 * @param EntityManagerInterface $em
+	 * @param Globals $globals
+	 */
+	public function __construct(EntityManagerInterface $em, Globals $globals)
+	{
+		$this->em = $em;
+		$this->globals = $globals;
+	}
+
+	/**
+	 * method that get entity of current movement based on the type and type id of it
+	 * @param int $type
+	 * @param int $type_id
+	 * @return object|null
+	 */
+	private function getEntityOfTypeMovement(int $type, int $type_id)
+	{
+		$entity = null;
+		if ($type === UnitMovement::TYPE_MISSION) {
+			$entity = \App\Entity\Mission::class;
+		}
+
+		if (!$entity) {
+			return null;
+		}
+
+		return $this->em->getRepository($entity)->find($type_id);
+	}
+
+	/**
+	 * method that return current units movements in base
+	 */
+	public function getCurrentMovementsInBase()
+	{
+		$unit_movements = $this->em->getRepository(UnitMovement::class)->findBy([
+			"base" => $this->globals->getCurrentBase()
+		]);
+		$return_movements = [];
+
+		foreach ($unit_movements as $unit_movement) {
+			$entity_type = $this->getEntityOfTypeMovement($unit_movement->getType(), $unit_movement->getTypeId());
+
+			$return_movements[] = [
+				"duration" => $unit_movement->getDuration(),
+				"end_date" => $unit_movement->getEndDate()->getTimestamp(),
+				"type" => $unit_movement->getType(),
+				"entity_type" => $entity_type,
+				"movement_type" => $unit_movement->getType(),
+				"units" => $unit_movement->getUnits()
+			];
+		}
+
+		return $return_movements;
+	}
+}
