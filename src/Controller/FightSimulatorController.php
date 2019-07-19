4a5af06e11e630ebc71c9adda2abae0a7e1468ed
Piou-piou <pilloud.anthony@gmail.com>
2019-07-19 08:25:37 +0200

    [refa] attackOrDefendUnit method

--- src/Controller/FightSimulatorController.php
+++ src/Controller/FightSimulatorController.php
@@ -60,6 +60,37 @@ class FightSimulatorController extends AbstractController
 	}
 
 	/**
+	 * method that put damage on unit when is in defense or get damage from a defense unit
+	 * @param Globals $globals
+	 * @param Unit $unit
+	 * @param array $units
+	 * @param string $type
+	 * @return array
+	 */
+	private function attackOrDefendUnit(Globals $globals, Unit $unit, array $units, string $type = "attack"): array
+	{
+		$units_config = $globals->getUnitsConfig();
+		$power = $units_config[$unit->getArrayName()][$type."_power"];
+		$key = count(array_keys($units)) > 0 ? array_keys($units)[0] : null;
+
+		if ($key !== null) {
+			$units[$key]->setLife($units[$key]->getLife() - $power);
+
+			if ($units[$key]->getLife() <= 0) {
+				$delete_for_next = abs($units[$key]->getLife());
+				unset($units[$key]);
+				$key = count(array_keys($units)) > 0 ? array_keys($units)[0] : null;
+
+				if ($key !== null) {
+					$units[$key]->setLife($units[$key]->getLife() - $delete_for_next);
+				}
+			}
+		}
+
+		return $units;
+	}
+
+	/**
 	 * @Route("/api/fight/simulate/", name="fight_simulate")
 	 * @param SessionInterface $session
 	 * @param Globals $globals
@@ -85,27 +116,5 @@ class FightSimulatorController extends AbstractController
 		}
 
 		return new JsonResponse();
-	}
-
-	private function attackOrDefendUnit(Globals $globals, Unit $unit, array $units, string $type = "attack") {
-		$units_config = $globals->getUnitsConfig();
-		$power = $units_config[$unit->getArrayName()][$type."_power"];
-		$key = count(array_keys($units)) > 0 ? array_keys($units)[0] : null;
-
-		if ($key !== null) {
-			$units[$key]->setLife($units[$key]->getLife() - $power);
-
-			if ($units[$key]->getLife() <= 0) {
-				$delete_for_next = abs($units[$key]->getLife());
-				unset($units[$key]);
-				$key = count(array_keys($units)) > 0 ? array_keys($units)[0] : null;
-
-				if ($key !== null) {
-					$units[$key]->setLife($units[$key]->getLife() - $delete_for_next);
-				}
-			}
-		}
-
-		return $units;
 	}
 }
