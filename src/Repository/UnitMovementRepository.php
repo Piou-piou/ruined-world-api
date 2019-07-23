2bc26ea059454baf9a1b0ce63f76258ddb97c48a
Piou-piou <pilloud.anthony@gmail.com>
2019-07-23 08:48:48 +0200

    [func] add method to get all movement of the base sent and received

--- src/Repository/UnitMovementRepository.php
+++ src/Repository/UnitMovementRepository.php
@@ -12,6 +12,23 @@ class UnitMovementRepository extends EntityRepository
 {
 
 	/**
+	 * method to get all movement of a base attack that are sent and attack tha wil be received
+	 * @param Base $base
+	 * @return mixed
+	 */
+	public function findMovementsByBase(Base $base)
+	{
+		$query = $this->getEntityManager()->createQuery("SELECT um FROM App:UnitMovement um
+			WHERE um.base = :base OR (um.type = :attack_type AND um.type_id = :base_id)
+		");
+		$query->setParameter("base", $base, Type::OBJECT);
+		$query->setParameter("attack_type", UnitMovement::TYPE_ATTACK, Type::INTEGER);
+		$query->setParameter("base_id", $base->getId(), Type::INTEGER);
+
+		return $query->getResult();
+	}
+
+	/**
 	 * method to find units that are currently in movement
 	 * @param UnitMovement $unitMovement
 	 * @return array
