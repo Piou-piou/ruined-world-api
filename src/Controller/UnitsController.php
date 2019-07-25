c305a44c160a0391b8e573eda66a49294a66a6c5
Piou-piou <pilloud.anthony@gmail.com>
2019-07-25 08:45:51 +0200

    [del] unused method

--- src/Controller/UnitsController.php
+++ src/Controller/UnitsController.php
@@ -14,22 +14,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class UnitsController extends AbstractController
 {
 	/**
-	 * method that send all units that exist in the game
-	 * @Route("/api/units/list-all-units/", name="units_list_all", methods={"POST"})
-	 * @param SessionInterface $session
-	 * @param Globals $globals
-	 * @return JsonResponse
-	 */
-	public function sendAllUnitsExistsInConfig(SessionInterface $session, Globals $globals): JsonResponse
-	{
-		return new JsonResponse([
-			"success" => true,
-			"token" => $session->get("user")->getToken(),
-			"units" => $globals->getUnitsConfig()
-		]);
-	}
-
-	/**
 	 * method that send units currently in base
 	 * @Route("/api/units/list-units-base/", name="units_list_base", methods={"POST"})
 	 * @param SessionInterface $session
