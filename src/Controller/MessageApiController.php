3b51a658ae8638e1c9f079fa1bac0a722640f9d2
Piou-piou <pilloud.anthony@gmail.com>
2019-07-26 08:28:27 +0200

    [func] add method to send received messages

--- src/Controller/MessageApiController.php
+++ src/Controller/MessageApiController.php
@@ -2,8 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\MessageBox;
+use App\Service\Api;
+use App\Service\Globals;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MessageApiController extends AbstractController
 {
+	/**
+	 * @Route("/api/message/list/", name="message_list", methods={"POST"})
+	 * @param Session $session
+	 * @param Globals $globals
+	 * @param Api $api
+	 * @return JsonResponse
+	 */
+	public function getMessagesOfBox(Session $session, Globals $globals, Api $api): JsonResponse
+	{
+		$user = $session->get("user");
+
+		$messages_box = $this->getDoctrine()->getManager()->getRepository(MessageBox::class)->findBy([
+			"user" => $user,
+			"type" => MessageBox::TYPE_RECEIVED
+		]);
+
+		return new JsonResponse([
+			"success" => true,
+			"token" => $session->get("user")->getToken(),
+			"messages" => $api->serializeObject($messages_box)
+		]);
+	}
 }
