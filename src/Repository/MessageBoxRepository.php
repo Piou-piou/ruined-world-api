c52bf58519b518204737970eddfe9190f7388ae0
Piou-piou <pilloud.anthony@gmail.com>
2019-07-29 20:06:35 +0200

    [func] add message box repository to get sent messages

---
+++ src/Repository/MessageBoxRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\ORM\EntityRepository;
+
+class MessageBoxRepository extends EntityRepository
+{
+	/**
+	 * @param User $user
+	 * @return mixed
+	 */
+	public function findBySentMessageBox(User $user)
+	{
+		$query = $this->getEntityManager()->createQuery("SELECT mb FROM App:MessageBox mb
+			JOIN App:Message m WITH m = mb.message AND m.user = :user
+		");
+		$query->setParameter("user", $user, Type::OBJECT);
+
+		return $query->getResult();
+	}
+}
