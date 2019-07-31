cf8b661316ae438ab644d87dfc4d7a44be2b39f3
Piou-piou <pilloud.anthony@gmail.com>
2019-07-31 19:01:02 +0200

    [refa] text in fight report

--- src/Service/FightReport.php
+++ src/Service/FightReport.php
@@ -81,12 +81,12 @@ class FightReport
 
 		$text = "<h2>rapport des unités envoyées</h2>";
 		foreach ($attack_units as $attack_unit) {
-			$text .= $attack_unit["name"] . " envoyés  : " . $attack_unit["return_number"] . " / " . $attack_unit["number"] . "<br>";
+			$text .= $attack_unit["name"] . " qui ont survécus  : " . $attack_unit["return_number"] . " / " . $attack_unit["number"] . "<br>";
 		}
 
 		$text .= "<h2>rapport des unités attaquées</h2>";
 		foreach ($defend_units as $defend_unit) {
-			$text .= $defend_unit["name"] . " attaquées  : " . $defend_unit["return_number"] . " / " . $defend_unit["number"] . "<br>";
+			$text .= $defend_unit["name"] . " tués  : " . $defend_unit["return_number"] . " / " . $defend_unit["number"] . "<br>";
 		}
 
 		$text .= "<h2>Ressources volées</h2>";
