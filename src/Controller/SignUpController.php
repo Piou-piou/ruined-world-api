db62e5edb605df3001ce570ec4aa40639dcf2460
Piou-piou <pilloud.anthony@gmail.com>
2019-08-07 08:12:14 +0200

    [func] prepare test method to check is pseudo is available

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -3,8 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SignUpController extends AbstractController
 {
 
+	/**
+	 * @Route("/api/signup/cjeck-pseudo-used/", name="signup_check_pseudo_used", methods={"POST"})
+	 * @return JsonResponse
+	 */
+	public function checkIfPseudoIsAvailable(): JsonResponse
+	{
+		return new JsonResponse([
+			"success" => true,
+			"used_pseudo" => true
+		]);
+	}
 }
