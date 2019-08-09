d8d7f50c32ac5c52ac96ea55c241d2cc2afa86aa
Piou-piou <pilloud.anthony@gmail.com>
2019-08-09 07:49:26 +0200

    [edit] method to validate a pseudo to validate mail too [func] add route for validate mail to exclude of api

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -10,24 +10,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SignUpController extends AbstractController
 {
+	private $error_message = "";
+
 	/**
 	 * method to check if pseudo is available
 	 * @Route("/api/signup/check-pseudo-used/", name="signup_check_pseudo_used", methods={"POST"})
+	 * @Route("/api/signup/check-mail-used/", name="signup_check_mail_used", methods={"POST"})
 	 * @param Request $request
 	 * @return JsonResponse
 	 */
 	public function checkIfPseudoIsAvailable(Request $request): JsonResponse
 	{
-		$user = $this->getDoctrine()->getManager()->getRepository(User::class)->findOneBy(["pseudo" => $request->get("pseudo")]);
-		$error_message = "";
+		$user_repo = $this->getDoctrine()->getManager()->getRepository(User::class);
 
-		if ($user) {
-			$error_message = "Ce pseudo est déjà utilisé, merci d'en choisir un autre";
+		if ($request->get("pseudo")) {
+			$user = $user_repo->findOneBy(["pseudo" => $request->get("pseudo")]);
+			$this->error_message = "Ce pseudo est déjà utilisé";
+		} else if ($request->get("mail")) {
+			$user = $user_repo->findOneBy(["mail" => $request->get("mail")]);
+			$this->error_message = "Cette adresse email est déjà utilisé";
+		}
+
+		if (!$user) {
+			$this->error_message = "";
 		}
 
 		return new JsonResponse([
 			"success" => true,
-			"error_message" => $error_message
+			"error_message" => $this->error_message
 		]);
 	}
 }
