da83de9a333a8b82c5a1b3c933b477be4de37821
Piou-piou <pilloud.anthony@gmail.com>
2019-09-11 08:02:41 +0200

    [func] add method to treat units

--- src/Controller/InfirmaryController.php
+++ src/Controller/InfirmaryController.php
@@ -4,11 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Unit;
 use App\Service\Api;
+use App\Service\Barrack;
 use App\Service\Globals;
 use App\Service\Infirmary;
+use App\Service\Point;
+use DateInterval;
 use Doctrine\Common\Annotations\AnnotationException;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
@@ -17,7 +22,7 @@ class InfirmaryController extends AbstractController
 {
 	/**
 	 * method that send units currently in base with life inferior to 100 to treat them
-	 * @Route("/api/units/list-units-to-treat/", name="units_list_to_treat", methods={"POST"})
+	 * @Route("/api/infirmary/list-units-to-treat/", name="infirmary_list_to_treat", methods={"POST"})
 	 * @param SessionInterface $session
 	 * @param Globals $globals
 	 * @param Api $api
@@ -32,6 +37,7 @@ class InfirmaryController extends AbstractController
 		$general_config = $globals->getGeneralConfig();
 		$units = $this->getDoctrine()->getRepository(Unit::class)->findBy([
 			"base" => $globals->getCurrentBase(),
+			"in_treatment" => false,
 			"in_recruitment" => false,
 			"unitMovement" => null
 		]);
@@ -71,5 +77,60 @@ class InfirmaryController extends AbstractController
 			"units" => $api->serializeObject($return_units),
 			"unit_config" => $globals->getUnitsConfig("units")
 		]);
+	}
+
+	/**
+	 * method to set time of end treatment of units
+	 * @Route("/api/infirmary/treat-units/", name="tinfirmary_reat_units", methods={"POST"})
+	 * @param Session $session
+	 * @param Globals $globals
+	 * @param Infirmary $infirmary
+	 * @param Point $point
+	 * @return JsonResponse
+	 * @throws Exception
+	 */
+	public function treatUnits(Session $session, Globals $globals, Infirmary $infirmary, Point $point): JsonResponse
+	{
+		$em = $this->getDoctrine()->getManager();
+		$now = new \DateTime();
+		$infos = $session->get("jwt_infos");
+		$unit_array_name = $infos->unit_array_name;
+		$number_to_treat = (int)$infos->number_to_recruit;
+		$treated_number = 0;
+
+		if ($infirmary->testWithdrawResourcesToTreat($unit_array_name, $number_to_treat)) {
+			$units = $em->getRepository(Unit::class)->findBy([
+				"base" => $globals->getCurrentBase(),
+				"in_treatment" => false,
+				"in_recruitment" => false,
+				"unitMovement" => null
+			])->orderBy(["life" => "ASC"]);
+
+			/** @var Unit $unit */
+			foreach ($units as $unit) {
+				if ($treated_number < $number_to_treat) {
+					$end_treatment = $now->add(new DateInterval("PT" . $infirmary->getTimeToTreat($unit->getArrayName()) . "S"));
+					$unit->setInTreatment(true);
+					$unit->setEndTreatment($end_treatment);
+					$em->persist($unit);
+
+					$treated_number++;
+				} else {
+					break;
+				}
+			}
+
+			$em->flush();
+
+			return new JsonResponse([
+				"success" => true,
+				"success_message" => "Les unités sélectionnées sont en cours de guérison"
+			]);
+		} else {
+			return new JsonResponse([
+				"success" => false,
+				"error_message" => "Il n'y a pas assez de ressources dans la base pour soigner autant d'unités"
+			]);
+		}
 	}
 }
