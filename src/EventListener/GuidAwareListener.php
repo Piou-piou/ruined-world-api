35266a27f1a8e0c299c7ad86609d618d9c1d5c27
Piou-piou <pilloud.anthony@gmail.com>
2020-09-24 08:52:03 +0200

    [refa] comment in guid aware listener

--- src/EventListener/GuidAwareListener.php
+++ src/EventListener/GuidAwareListener.php
@@ -3,6 +3,7 @@
 namespace App\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
@@ -13,19 +14,31 @@ class GuidAwareListener
 	 * @var ContainerInterface
 	 */
 	private $container;
-	
+
+	/**
+	 * GuidAwareListener constructor.
+	 * @param ContainerInterface $container
+	 */
 	public function __construct(ContainerInterface $container)
 	{
 		$this->container = $container;
 	}
-	
+
+	/**
+	 * @param $entity
+	 * @param LifecycleEventArgs $events
+	 */
 	public function prePersist($entity, LifecycleEventArgs $events)
 	{
 		if ($entity->getGuid() === null) {
 			$entity->setGuid((string)$this->generate());
 		}
 	}
-	
+
+	/**
+	 * @return UuidInterface
+	 * @throws \Exception
+	 */
 	private static function generate()
 	{
 		try {
