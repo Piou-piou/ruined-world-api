eddb7159432a765bb2b59191bfe5886f647935bf
Piou-piou <pilloud.anthony@gmail.com>
2020-10-09 08:51:57 +0200

    [func] prepare my league show

--- src/Controller/EmbassyController.php
+++ src/Controller/EmbassyController.php
@@ -2,31 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Building;
 use App\Entity\League;
 use App\Service\Api;
 use App\Service\Globals;
+use Doctrine\Common\Annotations\AnnotationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 
 class EmbassyController extends AbstractController
 {
 	/**
 	 * @Route("/api/embassy/show/", name="embassy_show", methods={"POST"})
 	 * @param SessionInterface $session
+	 * @param Api $api
+	 * @param Globals $globals
 	 * @return JsonResponse
+	 * @throws AnnotationException
+	 * @throws ExceptionInterface
 	 */
-	public function sendMyEmbassy(SessionInterface $session)
+	public function sendMyEmbassy(SessionInterface $session, Api $api, Globals $globals): JsonResponse
 	{
 		$em = $this->getDoctrine()->getManager();
 
 		$league = $em->getRepository(League::class)->findOneBy(["leader" => $session->get("user")]);
+		$embassy = $em->getRepository(Building::class)->findOneBy([
+			"array_name" => "embassy",
+			"base" => $globals->getCurrentBase()
+		]);
 
 		return new JsonResponse([
 			"success" => true,
 			"token" => $session->get("user_token")->getToken(),
-			"league" => $league
+			"league" => $api->serializeObject($league),
+			"embassy" => $api->serializeObject($embassy)
 		]);
 	}
 }
